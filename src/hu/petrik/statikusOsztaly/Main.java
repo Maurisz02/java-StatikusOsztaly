3513a53b56cf755bf894744670a992eebe7b4f0d
Maurisz02 <p2002adam@gmail.com>
2022-10-06 08:38:53 +0200

    alap

--- src/hu/petrik/statikusOsztaly/Main.java
+++ src/hu/petrik/statikusOsztaly/Main.java
@@ -1,7 +1,23 @@
 package hu.petrik.statikusOsztaly;
 
 public class Main {
+    int nemStatikusTag;
+    static int statikusTag;
+
+
+
+
     public static void main(String[] args) {
+
+        for(int i = 0; i < 100; i++){
+            System.out.println(Veletlen.velEgesz(5,10));
+        }
+
+        /*
+        Main r = new Main();
+        System.out.println(r.nemStatikusTag);
+        System.out.println(Main.statikusTag);
+        */
 
     }
 }
